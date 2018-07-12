c602519861ac4574c0362e3cba19bebb879e5432
MacPro <andersjuulrefslundpetersen@gmail.com>
2018-07-13 00:17:03 +0200

    first commit

---
+++ src/javaapplicationhtmlunit/Elev.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationhtmlunit;
+
+/**
+ *
+ * @author andersjuulr.petersen
+ */
+public class Elev {
+    public String navn  = "";
+    public double almF  = 0;
+    public double skrF  = 0;
+}
