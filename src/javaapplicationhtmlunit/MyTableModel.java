513bc8fff62df3dc355cc881777eadab23eff0ca
MacPro <andersjuulrefslundpetersen@gmail.com>
2018-07-17 23:49:28 +0200

    commit 2

---
+++ src/javaapplicationhtmlunit/MyTableModel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationhtmlunit;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author andersjuulr.petersen
+ */
+public class MyTableModel extends DefaultTableModel{
+
+    MyTableModel(String[] col, int i) {
+        super(col,i);
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        @Override
+        public Class getColumnClass(int column) {
+        
+        if (column == 0){
+            return String.class;
+        }else{
+            return Double.class;
+        }
+           
+        }
+        /*
+         List<Color> rowColours = Arrays.asList(
+        Color.RED,
+        Color.GREEN,
+        Color.CYAN
+    );
+
+    public void setRowColour(int row, Color c) {
+        rowColours.set(row, c);
+        fireTableRowsUpdated(row, row);
+    }
+
+    public Color getRowColour(int row) {
+        return rowColours.get(row);
+    }
+
+    @Override
+    public int getRowCount() {
+        return 3;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        return String.format("%d %d", row, column);
+    }
+*/
+    
+}
